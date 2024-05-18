b158a8593ffac33147130e50b02ea763d05371b3
SamiDevW <SamiDevW@users.noreply.github.com>
2024-05-19 00:38:53 +0200

    codeCleaning+responsive

--- navbar.js
+++ navbar.js
@@ -1,21 +1,4 @@
 
-function toggleOff() {
-  
-  let toggle = document.querySelector(".navbar-toggle");
-  let navLinks = document.querySelector(".nav-link");
-
-
-  if (toggle.style.display === "none") {
-    navLinks.style.setProperty("display", "flex", "important");
-    // navLinks.style.display = `"flex" !important`;
-  }
-
-}
-toggleOff ();
-window.addEventListener('resize', function() {
-  
-  toggleOff ();
-});
 
 function toggleNavbar() {
   
---
+++ navbarOn.js
@@ -0,0 +1,19 @@
+
+// function toggleOff() {
+  
+// //   let toggle = document.querySelector(".navbar-toggle");
+//   let navLinks = document.querySelector(".nav-link");
+//   let width = window.innerWidth;
+//   console.log(width)
+
+//   if (width > 1023 ) {
+//     navLinks.style.display = "flex";
+//   }
+  
+
+// }
+
+// window.addEventListener('resize', function() {
+  
+//   toggleOff();
+// });
